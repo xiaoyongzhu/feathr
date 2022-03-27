246133e43e3ec560fc425676dce82c4e17f1390e
Xiaoyong Zhu <xiaoyzhu@outlook.com>
2022-03-27 12:54:02 -0700

    Update pysparkudf.py

--- pysparkudf.py
+++ pysparkudf.py
@@ -1,12 +1,13 @@
-from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-from pyspark import SparkFiles
+from non_agg_features import *
 
-
-if __name__ == "__main__":
-  spark = SparkSession.builder.getOrCreate()
-  from non_agg_features import *
+def main(spark: SparkSession):
+  
   print(batch_source.name)
   df = spark.read.option('header', 'true').csv('wasbs://public@azurefeathrstorage.blob.core.windows.net/sample_data/green_tripdata_2020-04.csv')
   df = df.filter("tolls_amount > 0.0")
   df.write.mode('overwrite').csv('abfss://feathrazuretest3fs@feathrazuretest3storage.dfs.core.windows.net/UDF_output.csv')
+
+
+if __name__ == "__main__":
+  main(SparkSession.builder.getOrCreate())
