7ec738177b5d3549daaaa9ee58c231055c46d925
aabbasi-hbo <92401544+aabbasi-hbo@users.noreply.github.com>
2022-11-08 21:27:24 -0800

    sql processing

--- feathr_project/feathr/definition/settings.py
+++ feathr_project/feathr/definition/settings.py
@@ -21,7 +21,8 @@ class ObservationSettings(HoconConvertible):
         self.event_timestamp_column = event_timestamp_column
         self.timestamp_format = timestamp_format
         self.observation_path = observation_path
-        print(f"OBSERVATION PATH: {self.observation_path}")
+        if self.observation_path.startswith("sql%"):
+            self.observation_path = ' '.join(self.observation_path.splitlines())
         if observation_path.startswith("http"):
             logger.warning("Your observation_path {} starts with http, which is not supported. Consider using paths starting with wasb[s]/abfs[s]/s3.", observation_path)
 
